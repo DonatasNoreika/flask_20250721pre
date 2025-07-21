e4e15617682178149d4a0456d90c97a8b354457f
DonatasNoreika <donoras@gmail.com>
2025-07-21 16:24:41 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+import forms
+from flask_bcrypt import Bcrypt
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'registruotis'
+login_manager.login_message_category = 'info'
+
+
+class Vartotojas(db.Model, UserMixin):
+    __tablename__ = "vartotojas"
+    id = db.Column(db.Integer, primary_key=True)
+    vardas = db.Column("Vardas", db.String(20), unique=True, nullable=False)
+    el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
+    slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
+
+
+class Uzduotis(db.Model):
+    __tablename__ = "uzduotis"
+    id = db.Column(db.Integer, primary_key=True)
+    pavadinimas = db.Column("Pavadinimas", db.String)
+    vartotojas_id = db.Column(db.Integer, db.ForeignKey("vartotojas.id"))
+    vartotojas = db.relationship("Vartotojas")
+    atlikta = db.Column("Atlikta", db.Boolean)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/registruotis", methods=['GET', 'POST'])
+def registruotis():
+    db.create_all()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegistracijosForma()
+    if form.validate_on_submit():
+        koduotas_slaptazodis = bcrypt.generate_password_hash(form.slaptazodis.data).decode('utf-8')
+        vartotojas = Vartotojas(vardas=form.vardas.data, el_pastas=form.el_pastas.data,
+                                slaptazodis=koduotas_slaptazodis)
+        db.session.add(vartotojas)
+        db.session.commit()
+        flash('Sėkmingai prisiregistravote! Galite prisijungti', 'success')
+        return redirect(url_for('index'))
+    return render_template('registruotis.html', title='Register', form=form)
+
+
+@app.route("/prisijungti", methods=['GET', 'POST'])
+def prisijungti():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.PrisijungimoForma()
+    if form.validate_on_submit():
+        user = Vartotojas.query.filter_by(el_pastas=form.el_pastas.data).first()
+        if user and bcrypt.check_password_hash(user.slaptazodis, form.slaptazodis.data):
+            login_user(user, remember=form.prisiminti.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Prisijungti nepavyko. Patikrinkite el. paštą ir slaptažodį', 'danger')
+    return render_template('prisijungti.html', title='Prisijungti', form=form)
+
+
+@login_manager.user_loader
+def load_user(vartotojo_id):
+    return Vartotojas.query.get(int(vartotojo_id))
+
+
+@app.route("/atsijungti")
+def atsijungti():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/uzduotys")
+@login_required
+def uzduotys():
+    uzduotys = Uzduotis.query.filter_by(vartotojas=current_user).all()
+    return render_template('uzduotys.html', uzduotys=uzduotys)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
