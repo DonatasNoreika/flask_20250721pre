73ae35c6d3e254907e7cf50b021cf703d4934c5f
DonatasNoreika <donoras@gmail.com>
2025-07-23 14:06:26 +0300

    Nustatytas slaptažodžio pakeitimas su demo laišku

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from flask import Flask, render_template, redirect, url_for, flash, request
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask_sqlalchemy import SQLAlchemy
-import forms
 from flask_bcrypt import Bcrypt
+from flask_mail import Message, Mail
+import forms
 import os
 
 app = Flask(__name__)
@@ -18,6 +20,14 @@ login_manager.login_view = 'registruotis'
 login_manager.login_message_category = 'info'
 
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'MAIL_USERNAME'
+app.config['MAIL_PASSWORD'] = 'MAIL_PASSWORD'
+
+mail = Mail(app)
+
 class Vartotojas(db.Model, UserMixin):
     __tablename__ = "vartotojas"
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +35,18 @@ class Vartotojas(db.Model, UserMixin):
     el_pastas = db.Column("El. pašto adresas", db.String(120), unique=True, nullable=False)
     slaptazodis = db.Column("Slaptažodis", db.String(60), unique=True, nullable=False)
 
+    def get_reset_token(self):
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return Vartotojas.query.get(user_id)
 
 class Uzduotis(db.Model):
     __tablename__ = "uzduotis"
@@ -129,6 +151,51 @@ def trinti_uzduoti(id):
     db.session.commit()
     flash('Užduotis ištrinta!', 'success')
     return redirect(url_for('uzduotys'))
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Slaptažodžio atnaujinimo užklausa',
+                  sender='el@pastas.lt',
+                  recipients=[user.el_pastas])
+    msg.body = f'''Norėdami atnaujinti slaptažodį, paspauskite nuorodą:
+    {url_for('reset_token', token=token, _external=True)}
+    Jei jūs nedarėte šios užklausos, nieko nedarykite ir slaptažodis nebus pakeistas.
+    '''
+    print(msg.body)
+    # mail.send(msg)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = Vartotojas.verify_reset_token(token)
+    if user is None:
+        flash('Užklausa netinkama arba pasibaigusio galiojimo', 'warning')
+        return redirect(url_for('reset_request'))
+    form = forms.SlaptazodzioAtnaujinimoForma()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.slaptazodis.data).decode('utf-8')
+        user.slaptazodis = hashed_password
+        db.session.commit()
+        flash('Tavo slaptažodis buvo atnaujintas! Gali prisijungti', 'success')
+        return redirect(url_for('prisijungti'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = forms.UzklausosAtnaujinimoForma()
+    if form.validate_on_submit():
+        user = Vartotojas.query.filter_by(el_pastas=form.el_pastas.data).first()
+        send_reset_email(user)
+        flash('Jums išsiųstas el. laiškas su slaptažodžio atnaujinimo instrukcijomis.', 'info')
+        return redirect(url_for('prisijungti'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
 
 if __name__ == '__main__':
     with app.app_context():
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, BooleanField, StringField, PasswordField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Email
 import app
 
 
@@ -35,3 +35,18 @@ class UzduotisForma(FlaskForm):
     atlikta = BooleanField("Atlikta")
     submit = SubmitField('Įvesti')
 
+
+class UzklausosAtnaujinimoForma(FlaskForm):
+    el_pastas = StringField('El. paštas', validators=[DataRequired(), Email()])
+    submit = SubmitField('Gauti')
+
+    def validate_email(self, el_pastas):
+        user = app.Vartotojas.query.filter_by(el_pastas=el_pastas.data).first()
+        if user is None:
+            raise ValidationError('Nėra paskyros, registruotos šiuo el. pašto adresu. Registruokitės.')
+
+
+class SlaptazodzioAtnaujinimoForma(FlaskForm):
+    slaptazodis = PasswordField('Slaptažodis', validators=[DataRequired()])
+    patvirtintas_slaptazodis = PasswordField('Pakartokite slaptažodį', validators=[DataRequired(), EqualTo('slaptazodis')])
+    submit = SubmitField('Atnaujinti Slaptažodį')
